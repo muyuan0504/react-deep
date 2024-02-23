df3f654f13feb32eedbef7a09288271b85d8e8ae
aiden <972523881@qq.com>
2024-02-23 21:10:04 +0800

    fix

--- src/components/define-hook/util-hook.js
+++ src/components/define-hook/util-hook.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react'
 /**
  * 自定义 Hook 为了逻辑复用
  * 编写自定义hooks，要注意参数与返回值的一致性
+ *
+ * 自定义hook类似于将共同的存在相互依赖的的hooks数据封装到一个模块内，方便各个组件直接调用，可以理解为使用了hooks的封装函数
+ *
  * @returns
  */
 
