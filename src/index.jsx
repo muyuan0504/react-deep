95bbaa2ee807d089d3f4b6ec042e2dc16add657e
aiden <972523881@qq.com>
2024-04-16 18:07:47 +0800

    react-route

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import MyApp from './app'
+import { RouterProvider } from 'react-router-dom'
+import router from './route/index'
+
+// import MyApp from './app'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-    <React.StrictMode>
-        <MyApp />
-    </React.StrictMode>
+    <RouterProvider router={router} />
+    // <React.StrictMode>
+    //     <MyApp />
+    // </React.StrictMode>
 )
---
+++ src/route/index.jsx
@@ -0,0 +1,50 @@
+/**
+ * React 的web路由管理涉及两个库的使用 react-router 与 react-router-dom
+ *
+ * [react-router]：
+ * 是 React 官方提供的路由库，它提供了核心的路由功能，但不包含 DOM 相关的部分
+ * 提供了 <Route>、<Switch>、<Redirect> 等核心组件来定义路由。
+ *
+ * [react-router-dom]:
+ * 针对 Web 应用程序的库，它构建在 react-router 之上，并提供了与浏览器 DOM 相关的额外功能
+ * 包含了在 Web 应用中常用的组件，比如 <BrowserRouter>、<Link> 等，这些组件用于在应用中创建链接、处理浏览器历史记录等
+ */
+import * as React from 'react'
+import { createBrowserRouter, Link } from 'react-router-dom'
+
+import MyApp from '../app'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: (
+            <div>
+                <h1>Hello World</h1>
+                <Link to='about'>About Us</Link>
+            </div>
+        ),
+        children: [
+            {
+                path: '/about',
+                element: (
+                    <div>
+                        <h2>about</h2>
+                        <Link to='/about/concat'>About concat</Link>
+                    </div>
+                ),
+                children: [
+                    {
+                        path: 'concat',
+                        element: <div>about的子路由-concat</div>,
+                    },
+                ],
+            },
+            {
+                path: 'home',
+                element: <MyApp />,
+            },
+        ],
+    },
+])
+
+export default router
